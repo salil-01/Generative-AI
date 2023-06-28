96caec2913429e0e6abb79850a5b3807056bac6a
Salil Upadhyay <upsalil.01@gmail.com>
2023-06-28 12:49:11 +0530

    completed poem generator

--- poem-generator/index.js
+++ poem-generator/index.js
@@ -1,1 +1,41 @@
-//poem generator
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.post("/poem", async (req, res) => {
+  const keyword = req.body.keyword;
+  //   console.log(keyword);
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/engines/davinci/completions",
+      {
+        prompt: `Poem about ${keyword}`,
+        max_tokens: 100,
+        temperature: 0.7,
+        n: 1,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const poem = response.data.choices[0].text.trim();
+    res.json({ poem });
+  } catch (error) {
+    console.error("Error:", error.response.data);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
